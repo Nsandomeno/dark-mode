bbe4ee9ec7459d553827dd0bd7181ec65916a2a0
Nicholas Sandomeno <nicholassandomeno@Nicholass-Air.home>
2020-01-16 17:39:02 -0500

    new changes with local storage

--- src/components/hooks/useDarkMode.js
+++ src/components/hooks/useDarkMode.js
@@ -7,7 +7,7 @@ const [value, setValue] = useLocalStorage('darkMode')
     useEffect(() => {
         console.log("This is useEffect hook.")
         // check to see if the class is in localStorage
-        if (value === true) {
+        if (value === false) {
             // if value from localStorage is true, add it to the class of the body element
             document.body.classList.add('dark-mode')
         } else {
